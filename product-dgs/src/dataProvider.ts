7c031ba3291a6b92f90f4be20e5fcaed161293d2
mostofakamal <sumon154@gmail.com>
2025-04-20 23:33:48 +0200

    added replicas

--- product-dgs/src/dataProvider.ts
+++ product-dgs/src/dataProvider.ts
@@ -1,5 +1,7 @@
 export const getProducts = () => [
-  { id: '101', name: 'Laptop', description: 'A high-performance laptop' },
-  { id: '102', name: 'Headphones', description: 'Noise-cancelling headphones' },
-  { id: '103', name: 'Keyboard', description: 'Mechanical keyboard' },
+  { id: '101', name: 'Laptop', description: 'A high-performance laptop', launchDate: '2023-01-01' },
+  { id: '102', name: 'Smartphone', description: 'Latest model smartphone', launchDate: '2023-02-15' },
+  { id: '102', name: 'Headphones', description: 'Noise-cancelling headphones', launchDate: '2023-03-10' },
+  { id: '103', name: 'Monitor', description: '4K Ultra HD monitor', launchDate: '2023-04-20' }, 
+  { id: '103', name: 'Keyboard', description: 'Mechanical keyboard', launchDate: '2023-05-05' },
 ];
